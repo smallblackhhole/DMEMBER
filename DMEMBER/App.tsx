6e9fb1b0a759ac724d8c623391c3b55df3a5f7be
smallblackhhole <trungthinh10063009@gmail.com>
2023-05-16 16:02:17 +0700

    Revert "á"
    This reverts commit 9cee7f3a2bbbd1231654bba8847eb063c2297f55.

--- DMEMBER/App.tsx
+++ DMEMBER/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import { View,Text  } from "react-native";
 import Screnn from "./Screen/Screen";
 
@@ -10,14 +9,4 @@ return(
 </View>
 );
 };
-=======
-import React from 'react';
-import MenuAccount from './Menu Tài Khoản/MenuAccount';
-const App = () => {
-  return(
-    <MenuAccount/>
-  );
-}
-
->>>>>>> Stashed changes
 export default App;
